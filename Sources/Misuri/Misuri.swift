fc42c1640151b42d6c01e9079d6c28e6ed1b9157
Julio Litzenberg <jltzbrg@gmail.com>
2022-07-05 19:23:55 +0200

    Creating the MTimerProgressView

--- Sources/Misuri/Misuri.swift
+++ Sources/Misuri/Misuri.swift
@@ -1,10 +1,25 @@
-public struct Misuri {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
-    }
+public struct MTimerProgressView: View {
+    var progress: Int
+    var maxProgress: Int
+    var colors: [Color]
     
-    public func testingPackageWithGreeting() {
-        print("Its works!")
+    public var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 15)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+            
+            Circle()
+                .trim(from: 0.0, to: min(CGFloat((Double(progress) * Double(maxProgress))/100), 1.0))
+                .stroke(LinearGradient(colors: colors, startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle(degrees: 270))
+                .animation(Animation.linear(duration: Double(maxProgress)), value: progress)
+            
+            Text(String(progress))
+        }.frame(width: 150, height: 150)
     }
 }
+
--- Tests/MisuriTests/MisuriTests.swift
+++ Tests/MisuriTests/MisuriTests.swift
@@ -6,6 +6,6 @@ final class MisuriTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Misuri().text, "Hello, World!")
+        //XCTAssertEqual(Misuri().text, "Hello, World!")
     }
 }
